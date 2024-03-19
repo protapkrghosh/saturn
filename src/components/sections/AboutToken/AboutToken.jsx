887f3a0271407769dc1722083a4c1402f63bec37
Protap Ghosh <protapg518@gmail.com>
2024-03-20 00:08:04 +0600

    complete about section

--- src/components/sections/AboutToken/AboutToken.jsx
+++ src/components/sections/AboutToken/AboutToken.jsx
@@ -4,9 +4,9 @@ import { Button } from '@/components/ui/button';
 
 const AboutToken = () => {
   return (
-    <div className='relative'>
+    <div className='relative -mt-20 md:-mt-44 lg:mt-0 mb-28 lg:mb-0'>
       {/* Top path shadow */}
-      <div>
+      <div className='hidden lg:block'>
         <img src={pathBg} alt="Image" className='lg:w-[250px] xl:w-[300px] 2xl:w-[400px] absolute top-16 2xl:top-60 lg:left-[50%] xl:left-[45%] 2xl:left-[40%] opacity-[0.06]' />
 
         <div className='pathShadowTop  blur-[250px] 2xl:blur-[300px] w-[150px] xl:w-[250px] 2xl:w-[300px] h-[150px] xl:h-[250px] 2xl:h-[300px] absolute top-16 2xl:top-52 lg:left-[55%] xl:left-[45%] 2xl:left-[43%]' />
@@ -16,7 +16,7 @@ const AboutToken = () => {
         {/* About Contents */}
         <div className='lg:w-1/2'>
           <div className="max-w-[1620px] mx-auto xl:pl-24 lg:pl-16 md:pl-10 sm:pl-2 pl-4">
-            <h1 className='text-[#fff] text-[57px] lg:text-[44px] xl:text-[57px] 2xl:text-[58px] font-inter font-bold uppercase'>About <span className='text-[#40FE5B]'>$sat</span> token</h1>
+            <h1 className='text-[#fff] text-[50px] md:text-[57px] lg:text-[44px] xl:text-[57px] 2xl:text-[58px] font-inter font-bold uppercase'>About <span className='text-[#40FE5B]'>$sat</span> token</h1>
 
             <div className=' text-[#F4F4F4] text-[18px] font-inter font-normal space-y-4 mt-2 mb-10 2xl:w-[70%]'>
               <p>Introducing $SAT, the memecoin inspired by the legendary Sat Simpson! Known for his prowess in navigating the complexities of De-Fi with unparalleled coolness, Sat Simpson serves as the muse behind this innovative cryptocurrency.</p>
@@ -32,12 +32,12 @@ const AboutToken = () => {
 
         {/* Globe Image */}
         <div className='lg:w-1/2 flex justify-end'>
-          <img src={globe} alt="Image" className='w-full 2xl:w-[850px]' />
+          <img src={globe} alt="Image" className='w-full 2xl:w-[850px] -mb-32 md:-mb-52 lg:mb-0' />
         </div>
       </div>
 
       {/* bottom path shadow */}
-      <div>
+      <div className='hidden lg:block'>
         <img src={pathBg} alt="Image" className='lg:w-[250px] xl:w-[300px] 2xl:w-[400px] absolute lg:bottom-14 xl:bottom-44 2xl:bottom-72 left-[32%] opacity-[0.06] 2xl:opacity-[0.05]' />
 
         <div className='pathShadowBottom blur-[250px] 2xl:blur-[300px] w-[150px] xl:w-[200px] 2xl:w-[300px] h-[150px] xl:h-[200px] 2xl:h-[300px] absolute bottom-16 xl:bottom-36 2xl:bottom-52 left-[38%] xl:left-[36%] 2xl:left-[34%]' />
