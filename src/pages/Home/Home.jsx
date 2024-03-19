98f5adb65beb9ee79ccfd52a247262b7d6878b9d
Protap Ghosh <protapg518@gmail.com>
2024-03-19 13:12:43 +0600

    d

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,11 @@
+import Banner from '@/components/sections/Banner/Banner';
 import React from 'react';
 
 const Home = () => {
   return (
-    <div>
-      <h1 className='text-white'>This is home content</h1>
-    </div>
+    <>
+      <Banner />
+    </>
   );
 };
 
