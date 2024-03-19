ce6d5c9b2c0e56d812a55eb9c33323f5784ea30b
Protap Ghosh <protapg518@gmail.com>
2024-03-19 14:15:43 +0600

    d

--- src/components/sections/Banner/Banner.jsx
+++ src/components/sections/Banner/Banner.jsx
@@ -1,6 +1,7 @@
 import pathImg from '@/assets/banner/group.png'
 import shapeImg from '@/assets/banner/vector.png'
 import moonImg from '@/assets/banner/ellipse464.png'
+import symbols from '@/assets/banner/symbols.png'
 import Container from '../Container/Container';
 import { Button } from '@/components/ui/button';
 import { MdArrowOutward } from "react-icons/md";
@@ -16,10 +17,10 @@ const Banner = () => {
 
               <p className='text-[18px] text-white font-inter'>In the bustling city of Springfield, life pulses with energy and vibrancy. From the quaint streets lined with charming boutiques to the bustling squares filled with the laughter of families.</p>
 
-              <Button className="text-[#151414] rounded-[8px] group cursor-pointer md:px-5" data-aos="fade-up" data-aos-duration="3000">
+              <Button className="text-[#fff] bg-[#02B81C] rounded-[8px] group cursor-pointer px-8 py-6">
                 <span className="flex justify-center items-center">
-                  <p className='text-[16px] 2xl:text-[18px] font-inter'>Launch App</p>
-                  <MdArrowOutward className='text-[18px] ml-2 group-hover:-mt-2 duration-300' />
+                  <p className='text-[19px] font-inter font-normal uppercase mr-2'>Buy now</p>
+                  <img src={symbols} alt="Image" className='w-[30px] group-hover:-mt-2  duration-300' />
                 </span>
               </Button>
             </div>
