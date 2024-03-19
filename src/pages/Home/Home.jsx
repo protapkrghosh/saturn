582c8fe7a9168b1602a230da6594696c8305340b
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-03-19 13:01:00 +0600

    theset

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,10 @@
+import TheSatToken from '@/components/sections/TheSatToken/TheSatToken';
 import React from 'react';
 
 const Home = () => {
   return (
     <div>
-      <h1>This is home content</h1>
+      <TheSatToken />
     </div>
   );
 };
