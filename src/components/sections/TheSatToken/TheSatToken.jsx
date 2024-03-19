f22d6d5f3ee5c34547196387841d7c8b2708f5f5
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-03-19 13:51:29 +0600

    the sation

--- src/components/sections/TheSatToken/TheSatToken.jsx
+++ src/components/sections/TheSatToken/TheSatToken.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Container from '../Container/Container';
-import TheSatTokenImg from '@/assets/imges/theSatToken/Group 1000003510.png';
+import TheSatTokenImg from '@/assets/theSatToken/Group 1000003510.png'
+import { IoCheckmarkCircle } from "react-icons/io5";
 
 const TheSatToken = () => {
   return (
@@ -13,7 +14,23 @@ const TheSatToken = () => {
           </div>
           {/* THE SAT Token discription  */}
           <div>
-            <p className='text-[#FFF] font-'>THE SAT Token</p>
+            <p className='text-[#FFF] font-inter text-[58px] font-bold leading-[75.4px] '>THE <span className='text-[#40FE5B]'>SAT</span> Token</p>
+            <div className='flex items-center gap-4'>
+              <div><IoCheckmarkCircle className='text-[#40FE5B]' /></div>
+              <div><p className='text-[#F4F4F4] font-inter text-[18px] leading-[28.8px]'>In the heart of Springfield, where Lisa's wisdom met Homer's folly,</p></div>
+            </div>
+            <div className='flex items-center gap-4'>
+              <div><IoCheckmarkCircle className='text-[#40FE5B]' /></div>
+              <div><p className='text-[#F4F4F4] font-inter text-[18px] leading-[28.8px]'>$SAT token emerged, a crypto saga so jolly.</p></div>
+            </div>
+            <div className='flex items-center gap-4'>
+              <div><IoCheckmarkCircle className='text-[#40FE5B]' /></div>
+              <div><p className='text-[#F4F4F4] font-inter text-[18px] leading-[28.8px]'>A nod to Sat Simpson's mischief, it was born with a grin,</p></div>
+            </div>
+            <div className='flex items-center gap-4'>
+              <div><IoCheckmarkCircle className='text-[#40FE5B]' /></div>
+              <div><p className='text-[#F4F4F4] font-inter text-[18px] leading-[28.8px]'>For a playful, decentralized world, where the fun would begin</p></div>
+            </div>
           </div>
         </div>
       </Container>
