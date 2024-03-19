06e8c8c26ffdc93749bbc0c6a7d3efa92bb668b8
Protap Ghosh <protapg518@gmail.com>
2024-03-19 14:32:39 +0600

    d

--- src/components/sections/Banner/Banner.jsx
+++ src/components/sections/Banner/Banner.jsx
@@ -15,14 +15,16 @@ const Banner = () => {
             <div>
               <h1 className='text-[100px] text-white text-center font-bebasNeue font-normal tracking-wider uppercase'>SPRINGFIELD AND <span className='text-[#40FE5B]'>$SAT</span></h1>
 
-              <p className='text-[18px] text-white font-inter'>In the bustling city of Springfield, life pulses with energy and vibrancy. From the quaint streets lined with charming boutiques to the bustling squares filled with the laughter of families.</p>
+              <p className='text-[18px] text-white font-inter text-center w-full xl:w-[71%] 2xl:w-[58%] mx-auto mb-12'>In the bustling city of Springfield, life pulses with energy and vibrancy. From the quaint streets lined with charming boutiques to the bustling squares filled with the laughter of families.</p>
 
-              <Button className="text-[#fff] bg-[#02B81C] rounded-[8px] group cursor-pointer px-8 py-6">
-                <span className="flex justify-center items-center">
-                  <p className='text-[19px] font-inter font-normal uppercase mr-2'>Buy now</p>
-                  <img src={symbols} alt="Image" className='w-[30px] group-hover:-mt-2  duration-300' />
-                </span>
-              </Button>
+              <div className='flex justify-center'>
+                <Button className="text-[#fff] bg-[#02B81C] rounded-[8px] group cursor-pointer px-12 py-[27px]">
+                  <span className="flex justify-center items-center">
+                    <p className='text-[18px] 2xl:text-[19px] font-inter font-normal uppercase mr-2'>Buy now</p>
+                    <img src={symbols} alt="Image" className='w-[27px] 2xl:w-[28px] group-hover:-mt-2  duration-300' />
+                  </span>
+                </Button>
+              </div>
             </div>
           </Container>
         </div>
