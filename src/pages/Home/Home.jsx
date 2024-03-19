ef0f9ad14bed250f16b4343521d31e5f062b5613
Protap Ghosh <protapg518@gmail.com>
2024-03-19 22:37:17 +0600

    added about token

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+import AboutToken from '@/components/sections/AboutToken/AboutToken';
 import Banner from '@/components/sections/Banner/Banner';
 import TheSatToken from '@/components/sections/TheSatToken/TheSatToken';
 import React from 'react';
@@ -7,6 +8,7 @@ const Home = () => {
     <>
       <Banner />
       <TheSatToken />
+      <AboutToken />
     </>
   );
 };
