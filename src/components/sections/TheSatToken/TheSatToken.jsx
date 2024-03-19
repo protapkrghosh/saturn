323ec584271cd803136639bde3dcb59464fe456f
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-03-19 13:11:44 +0600

    theset

--- src/components/sections/TheSatToken/TheSatToken.jsx
+++ src/components/sections/TheSatToken/TheSatToken.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import Container from '../Container/Container';
+import TheSatTokenImg from '@/assets/imges/theSatToken/Group 1000003510.png';
 
 const TheSatToken = () => {
   return (
-    <div>
-      <h1>hello</h1>
+    <div className='bg-[#040D1B]'>
+      <Container>
+        <div>
+          {/* THE SAT Token images  */}
+          <div>
+            <img src={TheSatTokenImg} alt="" />
+          </div>
+          {/* THE SAT Token discription  */}
+          <div>
+            <p className='text-[#FFF] font-'>THE SAT Token</p>
+          </div>
+        </div>
+      </Container>
     </div>
   );
 };
