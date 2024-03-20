fef5816f6acaae15ea9a95394af9c64ba038daff
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-03-20 09:52:33 +0600

    new imge set

--- src/components/sections/Tokenomics/Tokenomics.jsx
+++ src/components/sections/Tokenomics/Tokenomics.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import Container from '../Container/Container';
-import tokenomicsImg from '@/assets/Tokenomics/Group 1000003458.png';
-import tokenomicsImg1 from '@/assets/Tokenomics/image 198.png';
-import tokenomicsImg2 from '@/assets/Tokenomics/image_197-removebg-preview (1) 2.png';
+import tokenomicsImg from '@/assets/Tokenomics/Group 1000003459.png';
+import tokenomicsImg1 from '@/assets/Tokenomics/image_197-removebg-preview (1) 2.png';
 import tokenomicsIcon1 from '@/assets/Tokenomics/Group 1000003460.png';
 
 const Tokenomics = () => {
@@ -13,8 +12,7 @@ const Tokenomics = () => {
           {/* imges section  */}
           <div className='w-[50%]'>
             <img src={tokenomicsImg} alt="" />
-            <img className='w-[15.5%] absolute top-[238px] left-[237px]' src={tokenomicsImg1} alt="" />
-            <img className='absolute top-[302px] left-[270px]' src={tokenomicsImg2} alt="" />
+            <img className='absolute top-[302px] left-[270px]' src={tokenomicsImg1} alt="" />
           </div>
           {/* catagory section  */}
           <div className='w-[50%]'>
