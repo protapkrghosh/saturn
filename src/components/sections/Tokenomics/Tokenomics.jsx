88399fe648d8ba4c8416b275df41b750267e5ba2
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-03-20 09:58:48 +0600

    token imges change

--- src/components/sections/Tokenomics/Tokenomics.jsx
+++ src/components/sections/Tokenomics/Tokenomics.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import Container from '../Container/Container';
-import tokenomicsImg from '@/assets/Tokenomics/Group 1000003459.png';
-import tokenomicsImg1 from '@/assets/Tokenomics/image_197-removebg-preview (1) 2.png';
-import tokenomicsIcon1 from '@/assets/Tokenomics/Group 1000003460.png';
+import tokenRound from '@/assets/Tokenomics/Group 1000003459.png';
+import earth from '@/assets/Tokenomics/image_197-removebg-preview (1) 2.png';
+import tokenIcon1 from '@/assets/Tokenomics/Group 1000003460.png';
 
 const Tokenomics = () => {
   return (
@@ -11,8 +11,8 @@ const Tokenomics = () => {
         <div className='flex relative'>
           {/* imges section  */}
           <div className='w-[50%]'>
-            <img src={tokenomicsImg} alt="" />
-            <img className='absolute top-[302px] left-[270px]' src={tokenomicsImg1} alt="" />
+            <img src={tokenRound} alt="" />
+            <img className='absolute top-[302px] left-[270px]' src={earth} alt="" />
           </div>
           {/* catagory section  */}
           <div className='w-[50%]'>
@@ -22,7 +22,7 @@ const Tokenomics = () => {
             <div className='flex justify-between items-center'>
               <div className='flex items-center gap-2'>
                 <div>
-                  <img src={tokenomicsIcon1} alt="imges" />
+                  <img src={tokenIcon1} alt="imges" />
                 </div>
                 <div>
                   <p className='text-[#FFF] font-inter text-[25px] font-bold'>Liquidity  Pool</p>
