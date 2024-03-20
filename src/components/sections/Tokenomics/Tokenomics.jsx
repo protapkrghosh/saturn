c70d3260296204dbabce3fdb758c84001d56a58c
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-03-20 17:10:15 +0600

    token

--- src/components/sections/Tokenomics/Tokenomics.jsx
+++ src/components/sections/Tokenomics/Tokenomics.jsx
@@ -12,11 +12,11 @@ const Tokenomics = () => {
   return (
     <>
       <Container>
-        <div className='flex relative'>
+        <div className='lg:flex relative'>
           {/* imges section  */}
-          <div className='w-[50%]'>
-            <img className='-ml-24' src={tokenRound} alt="image" />
-            <img className='absolute 2xl:top-[300px] xl:top-[245px] lg:top-[192px] 2xl:left-[175px] xl:left-[120px] lg:left-[80px] xl:w-auto lg:w-24' src={earth} alt="" />
+          <div className='lg:w-[50%] md:w-[80%]'>
+            <img className='lg:-ml-24 md:ml-20' src={tokenRound} alt="image" />
+            <img className='absolute 2xl:top-[300px] xl:top-[245px] lg:top-[192px] md:top-[235px] 2xl:left-[175px] xl:left-[120px] lg:left-[80px] md:left-[290px] xl:w-auto lg:w-24 md:w-32' src={earth} alt="" />
           </div>
           {/* catagory section  */}
           <div className='w-[50%]'>
