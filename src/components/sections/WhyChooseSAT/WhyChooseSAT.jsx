fbc9f2d948df73b6c7fe56d4b860fa6fa39e70fa
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-03-21 02:07:39 +0600

    w

--- src/components/sections/WhyChooseSAT/WhyChooseSAT.jsx
+++ src/components/sections/WhyChooseSAT/WhyChooseSAT.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Container from '../Container/Container';
 import whyChooseImg from '@/assets/whyChooseSAT/Group_1000003467.png'
+import whyChooseIcon from '@/assets/whyChooseSAT/image_197-removebg-preview (1) 1.png'
 import whyChooseIcon1 from '@/assets/whyChooseSAT/image 205.png'
 import whyChooseIcon2 from '@/assets/whyChooseSAT/Black.png'
 
@@ -12,9 +13,10 @@ const WhyChooseSAT = () => {
         <div className='flex gap-[50px]'>
           {/* why choose sat  */}
           <div className='bg-[#06201bab] border border-[#074933] rounded-[15px] w-1/2'>
-            <div>
+            <div className='relative'>
               <p className='text-[#FFF] font-inter text-[60px] font-bold mt-[80px] ml-20'>Why Choose <span className='text-[#40FE5B]'>SAT</span></p>
               <img className='mt-[114px] ml-10' src={whyChooseImg} alt="" />
+              <img className='absolute 2xl:top-[345px] 2xl:left-[205px]' src={whyChooseIcon} alt="" />
             </div>
           </div>
           {/* why choose two part  */}
