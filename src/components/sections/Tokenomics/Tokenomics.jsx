a2301142994f6d4377437aa862058110ae8f0ae3
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-03-20 16:26:46 +0600

    container

--- src/components/sections/Tokenomics/Tokenomics.jsx
+++ src/components/sections/Tokenomics/Tokenomics.jsx
@@ -11,90 +11,91 @@ import tokenIcon5 from '@/assets/Tokenomics/Group 1000003464.png';
 const Tokenomics = () => {
   return (
     <>
-      <div className='flex relative'>
-        {/* imges section  */}
-        <div className='w-[50%]'>
-          <img className='' src={tokenRound} alt="" />
-          <img className='absolute 2xl:top-[302px] xl:top-[245px] lg:top-[180px] 2xl:left-[270px] xl:left-[215px] lg:left-[150px]' src={earth} alt="" />
+      <Container>
+        <div className='flex relative'>
+          {/* imges section  */}
+          <div className='w-[50%]'>
+            <img className='' src={tokenRound} alt="" />
+            <img className='absolute 2xl:top-[302px] xl:top-[245px] lg:top-[180px] 2xl:left-[270px] xl:left-[215px] lg:left-[150px]' src={earth} alt="" />
+          </div>
+          {/* catagory section  */}
+          <div className='w-[50%]'>
+            <p className='text-[#FFF] font-inter text-[58px] font-bold uppercase'>Tokenomics</p>
+            {/* all catagory section  */}
+            {/* catagory 1  */}
+            <div className='flex justify-between items-center mb-8'>
+              <div className='flex items-center gap-2'>
+                <div>
+                  <img src={tokenIcon1} alt="imges" />
+                </div>
+                <div>
+                  <p className='text-[#FFF] font-inter text-[25px] font-bold'>Liquidity  Pool</p>
+                  <p className='text-[#FFF] font-inter text-[15px]'>35 billion $BART</p>
+                </div>
+              </div>
+              <div>
+                <p className='text-[#FFF] font-inter text-[25px] '>35 %</p>
+              </div>
+            </div>
+            <div className='flex justify-between items-center mb-8'>
+              <div className='flex items-center gap-2'>
+                <div>
+                  <img src={tokenIcon2} alt="imges" />
+                </div>
+                <div>
+                  <p className='text-[#FFF] font-inter text-[25px] font-bold'>Presale</p>
+                  <p className='text-[#FFF] font-inter text-[15px]'>30 billion $BART</p>
+                </div>
+              </div>
+              <div>
+                <p className='text-[#FFF] font-inter text-[25px] '>30 %</p>
+              </div>
+            </div>
+            <div className='flex justify-between items-center mb-8'>
+              <div className='flex items-center gap-2'>
+                <div>
+                  <img src={tokenIcon3} alt="imges" />
+                </div>
+                <div>
+                  <p className='text-[#FFF] font-inter text-[25px] font-bold'>Future development</p>
+                  <p className='text-[#FFF] font-inter text-[15px]'>20 billion $BART</p>
+                </div>
+              </div>
+              <div>
+                <p className='text-[#FFF] font-inter text-[25px] '>20 %</p>
+              </div>
+            </div>
+            <div className='flex justify-between items-center mb-8'>
+              <div className='flex items-center gap-2'>
+                <div>
+                  <img src={tokenIcon4} alt="imges" />
+                </div>
+                <div>
+                  <p className='text-[#FFF] font-inter text-[25px] font-bold'>Staking  rewards</p>
+                  <p className='text-[#FFF] font-inter text-[15px]'>10 billion $BART</p>
+                </div>
+              </div>
+              <div>
+                <p className='text-[#FFF] font-inter text-[25px] '>10 %</p>
+              </div>
+            </div>
+            <div className='flex justify-between items-center'>
+              <div className='flex items-center gap-2'>
+                <div>
+                  <img src={tokenIcon5} alt="imges" />
+                </div>
+                <div>
+                  <p className='text-[#FFF] font-inter text-[25px] font-bold'>Team</p>
+                  <p className='text-[#FFF] font-inter text-[15px]'>5 billion $BART</p>
+                </div>
+              </div>
+              <div>
+                <p className='text-[#FFF] font-inter text-[25px] '>5 %</p>
+              </div>
+            </div>
+          </div>
         </div>
-        {/* catagory section  */}
-        <div className='w-[50%]'>
-          <p className='text-[#FFF] font-inter text-[58px] font-bold uppercase'>Tokenomics</p>
-          {/* all catagory section  */}
-          {/* catagory 1  */}
-          <div className='flex justify-between items-center mb-8'>
-            <div className='flex items-center gap-2'>
-              <div>
-                <img src={tokenIcon1} alt="imges" />
-              </div>
-              <div>
-                <p className='text-[#FFF] font-inter text-[25px] font-bold'>Liquidity  Pool</p>
-                <p className='text-[#FFF] font-inter text-[15px]'>35 billion $BART</p>
-              </div>
-            </div>
-            <div>
-              <p className='text-[#FFF] font-inter text-[25px] '>35 %</p>
-            </div>
-          </div>
-          <div className='flex justify-between items-center mb-8'>
-            <div className='flex items-center gap-2'>
-              <div>
-                <img src={tokenIcon2} alt="imges" />
-              </div>
-              <div>
-                <p className='text-[#FFF] font-inter text-[25px] font-bold'>Presale</p>
-                <p className='text-[#FFF] font-inter text-[15px]'>30 billion $BART</p>
-              </div>
-            </div>
-            <div>
-              <p className='text-[#FFF] font-inter text-[25px] '>30 %</p>
-            </div>
-          </div>
-          <div className='flex justify-between items-center mb-8'>
-            <div className='flex items-center gap-2'>
-              <div>
-                <img src={tokenIcon3} alt="imges" />
-              </div>
-              <div>
-                <p className='text-[#FFF] font-inter text-[25px] font-bold'>Future development</p>
-                <p className='text-[#FFF] font-inter text-[15px]'>20 billion $BART</p>
-              </div>
-            </div>
-            <div>
-              <p className='text-[#FFF] font-inter text-[25px] '>20 %</p>
-            </div>
-          </div>
-          <div className='flex justify-between items-center mb-8'>
-            <div className='flex items-center gap-2'>
-              <div>
-                <img src={tokenIcon4} alt="imges" />
-              </div>
-              <div>
-                <p className='text-[#FFF] font-inter text-[25px] font-bold'>Staking  rewards</p>
-                <p className='text-[#FFF] font-inter text-[15px]'>10 billion $BART</p>
-              </div>
-            </div>
-            <div>
-              <p className='text-[#FFF] font-inter text-[25px] '>10 %</p>
-            </div>
-          </div>
-          <div className='flex justify-between items-center'>
-            <div className='flex items-center gap-2'>
-              <div>
-                <img src={tokenIcon5} alt="imges" />
-              </div>
-              <div>
-                <p className='text-[#FFF] font-inter text-[25px] font-bold'>Team</p>
-                <p className='text-[#FFF] font-inter text-[15px]'>5 billion $BART</p>
-              </div>
-            </div>
-            <div>
-              <p className='text-[#FFF] font-inter text-[25px] '>5 %</p>
-            </div>
-          </div>
-        </div>
-      </div>
-
+      </Container>
     </>
   );
 };
