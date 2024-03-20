850950880be48bcd46ad41d6ef704a9b893b82ce
Protap Ghosh <protapg518@gmail.com>
2024-03-21 00:30:27 +0600

    d

--- src/components/sections/NewUnlites/NewUnlites.jsx
+++ src/components/sections/NewUnlites/NewUnlites.jsx
@@ -17,15 +17,15 @@ const NewUnlites = () => {
         <div className='lg:w-1/2 flex justify-start relative'>
           <img src={globe} alt="Image" className='xl:w-auto 2xl:w-[480px] xl:h-[510px] 2xl:h-auto -mb-32 md:-mb-52 lg:mb-0 mix-blend-screen' />
 
-          <img src={earthImg} alt="Image" className='w-[110px] md:w-[200px] lg:w-[140px] xl:w-[190px] 2xl:w-[240px] absolute bottom-[200px] md:bottom-[378px] lg:bottom-[387px] xl:bottom-[420px] 2xl:bottom-[265px] left-4 md:left-7 lg:left-3 xl:left-5 2xl:left-9' />
+          <img src={earthImg} alt="Image" className='w-[110px] md:w-[200px] lg:w-[140px] xl:w-[190px] 2xl:w-[240px] absolute bottom-[200px] md:bottom-[378px] lg:bottom-[387px] xl:bottom-[365px] 2xl:bottom-[265px] left-4 md:left-7 lg:left-3 xl:left-5 2xl:left-9' />
         </div>
 
         {/* Sat wear */}
-        <div className="w-3/3 2xl:w-3/3">
+        <div className="lg:2/3 xl:w-3/3 2xl:w-3/3">
           <div className="max-w-[1620px] mx-auto 2xl:pr-[250px] xl:pr-24 lg:pr-16 md:pr-10 sm:pr-2 pr-4">
             {/* Section one */}
             <div className="border-y border-[#074933] py-10">
-              <div className="flex justify-between gap-x-10 2xl:gap-x-64 ml-16">
+              <div className="flex justify-between gap-x-32 2xl:gap-x-64 ml-16">
                 <div className="font-inter">
                   <h3 className="text-[#FFF] text-[25px] mb-6">SAT Wear</h3>
                   <p className="text-[#a6aaae] text-[17.5px]">Effortlessly trade $SAT tokens on our decentralized exchange.</p>
@@ -40,7 +40,7 @@ const NewUnlites = () => {
 
             {/* Section Two */}
             <div className="border-y border-[#074933] py-10 my-12">
-              <div className="flex justify-between gap-x-64 ml-16">
+              <div className="flex justify-between gap-x-32 2xl:gap-x-64 ml-16">
                 <div className="font-inter">
                   <h3 className="text-[#FFF] text-[25px] mb-6">Voting</h3>
                   <p className="text-[#a6aaae] text-[17.5px]">Effortlessly trade $SAT tokens on our decentralized exchange.</p>
@@ -55,7 +55,7 @@ const NewUnlites = () => {
 
             {/* Section Three */}
             <div className="border-y border-[#074933] py-10 relative">
-              <div className="flex justify-between gap-x-64 ml-16 z-30">
+              <div className="flex justify-between gap-x-32 2xl:gap-x-64 ml-16 z-30">
                 <div className="font-inter">
                   <h3 className="text-[#FFF] text-[25px] mb-6">SAT Bridge</h3>
                   <p className="text-[#a6aaae] text-[17.5px]">Effortlessly trade $SAT tokens on our decentralized exchange.</p>
