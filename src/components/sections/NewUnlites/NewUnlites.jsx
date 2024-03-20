26bf9f9c66570502fefd8b5ceb509dea982dff0c
Protap Ghosh <protapg518@gmail.com>
2024-03-20 23:04:40 +0600

    update hodi section

--- src/components/sections/NewUnlites/NewUnlites.jsx
+++ src/components/sections/NewUnlites/NewUnlites.jsx
@@ -4,20 +4,22 @@ import earthImg from "@/assets/unlitesAwait/image1.png"
 
 const NewUnlites = () => {
   return (
-    <div>
+    <div className="mt-32">
       <Container>
         <h1 className='text-[#fff] text-[58px] text-center font-inter font-bold'>Beyond memes new <span className="text-[#40FE5B]">unlites</span> await</h1>
       </Container>
 
       {/* Content area */}
-      <div className="lg:flex justify-between">
+      <div className="lg:flex justify-between -mt-60">
         {/* Globe Image */}
         <div className='lg:w-1/2 flex justify-start relative'>
           <img src={globe} alt="Image" className='w-full 2xl:w-[850px] -mb-32 md:-mb-52 lg:mb-0' />
-          <img src={earthImg} alt="Image" className='w-[110px] md:w-[200px] lg:w-[140px] xl:w-[190px] 2xl:w-[240px] absolute bottom-[200px] md:bottom-[378px] lg:bottom-[387px] xl:bottom-[508px] 2xl:bottom-[640px] left-4 md:left-7 lg:left-3 xl:left-3 2xl:left-5 animate-spin duration-2000' />
+          <img src={earthImg} alt="Image" className='w-[110px] md:w-[200px] lg:w-[140px] xl:w-[190px] 2xl:w-[240px] absolute bottom-[200px] md:bottom-[378px] lg:bottom-[387px] xl:bottom-[508px] 2xl:bottom-[640px] left-4 md:left-7 lg:left-3 xl:left-3 2xl:left-5' />
         </div>
 
-        <div></div>
+        <div>
+
+        </div>
       </div>
     </div>
   );
