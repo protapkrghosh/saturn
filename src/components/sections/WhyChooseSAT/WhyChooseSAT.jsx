c4021d154e512fc3e174e0295344b633ab69a19a
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-03-22 02:42:59 +0600

    a

--- src/components/sections/WhyChooseSAT/WhyChooseSAT.jsx
+++ src/components/sections/WhyChooseSAT/WhyChooseSAT.jsx
@@ -15,7 +15,7 @@ const WhyChooseSAT = () => {
           <div className='bg-[#06201b10] border border-[#074933] border-b-0 rounded-[15px] rounded-b-none w-1/2'>
             <div className='relative'>
               <p className='text-[#FFF] font-inter text-[55px] 2xl:text-[60px] font-bold mt-[80px] 2xl:ml-20 xl:ml-7'>Why Choose <span className='text-[#40FE5B]'>$SAT</span></p>
-              <img className='absolute mt-[200px] 2xl:w-[600px] xl:w-[475px] lg:w-[335px] 2xl:top-[0px] 2xl:left-[40px]' src={whyChooseImg} alt="" />
+              <img className='absolute 2xl:w-[650px] xl:w-[475px] lg:w-[335px] 2xl:top-[170px] xl:top-[90px] 2xl:left-[15px] xl:left-[30px]' src={whyChooseImg} alt="" />
 
             </div>
           </div>
