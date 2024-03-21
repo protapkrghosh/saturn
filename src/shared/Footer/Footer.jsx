c362eee18895322dfb3873860aa749c6fa389ef0
Protap Ghosh <protapg518@gmail.com>
2024-03-21 23:38:42 +0600

    added footer

--- src/shared/Footer/Footer.jsx
+++ src/shared/Footer/Footer.jsx
@@ -1,10 +1,16 @@
 import Container from '@/components/sections/Container/Container';
-import React from 'react';
+import circleImg from '@/assets/footer/Group21.png';
+import pathImg from '@/assets/aboutToken/group.png';
 
 const Footer = () => {
   return (
     <div>
-      <h1 className='text-white'>This is footer</h1>
+      <div className='relative'>
+        {/* <div style={{ backgroundImage: `url(${pathImg})` }} className='w-[429px] h-[260px] absolute bottom-36' /> */}
+
+        <img src={circleImg} alt="Image" className='mx-auto' />
+        <p className='text-[#FFF] text-[50px] 2xl:text-[55px] font-inter font-semibold uppercase -mt-[500px] 2xl:-mt-[550px] text-center xl:w-[64%] 2xl:w-[50%] mx-auto'>JOIN SAT AND LETS BARK OUR  WAY TO CRYPTO STARDOM  TOGETHER!</p>
+      </div>
     </div>
   );
 };
