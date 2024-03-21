020011a10a260ba02d167a311f59ddedf2c05707
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-03-22 03:53:18 +0600

    a

--- src/components/sections/WhyChooseSAT/WhyChooseSAT.jsx
+++ src/components/sections/WhyChooseSAT/WhyChooseSAT.jsx
@@ -10,12 +10,12 @@ const WhyChooseSAT = () => {
       <div className='whyChooseSAT w-[400px] h-[1000px] absolute -top-52 -left-52 ' />
       <Container>
         {/* chooes sat  */}
-        <div className='grid grid-cols-2 gap-[50px]'>
+        <div className='grid lg:grid-cols-2 md:grid-cols-1 gap-[50px]'>
           {/*  why choose sat  */}
           <div className='bg-[#06201b10] border border-[#074933] border-b-0 rounded-[15px] rounded-b-none'>
             <div className='relative'>
               <p className='text-[#FFF] font-inter text-[60px] font-bold mt-[81px] text-center lg:px-2'>Why Choose <span className='text-[#40FE5B]'>$SAT</span></p>
-              <img className='absolute 2xl:w-[650px] xl:w-[510px] lg:w-[375px] 2xl:top-[170px] xl:top-[265px] lg:top-[415px] 2xl:left-[15px] xl:left-[20px] lg:left-[20px]' src={whyChooseImg} alt="" />
+              <img className='mx-auto lg:absolute 2xl:w-[650px] xl:w-[510px] lg:w-[375px] 2xl:top-[170px] xl:top-[265px] lg:top-[415px] 2xl:left-[15px] xl:left-[20px] lg:left-[20px]' src={whyChooseImg} alt="" />
 
             </div>
           </div>
