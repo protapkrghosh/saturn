f969217476775e9c055b49e08705c701d73cca75
Protap Ghosh <protapg518@gmail.com>
2024-04-05 16:32:59 +0600

    update timer

--- src/components/sections/Banner/Banner.jsx
+++ src/components/sections/Banner/Banner.jsx
@@ -28,8 +28,8 @@ const Banner = () => {
     if (difference > 0) {
       timeLeft = {
         // days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-        hours: Math.floor((difference / (500 * 60 * 60)) % 24),
-        minutes: Math.floor((difference / 600 / 60) % 60),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
         seconds: Math.floor((difference / 1000) % 60),
       };
     }
